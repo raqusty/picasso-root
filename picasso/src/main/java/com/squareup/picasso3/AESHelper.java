9f43d7d7b24cc4a767019f3995f8ebac9dd0d549
raqusty <583006809@qq.com>
2019-08-05 19:20:49 +0800

    picasso 图片加密

--- picasso/src/main/java/com/squareup/picasso3/AESHelper.java
+++ picasso/src/main/java/com/squareup/picasso3/AESHelper.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -31,26 +33,31 @@ public class AESHelper {
     }
 
     public static byte[] encrypt(byte[] messageBytes, byte[] psBytes) throws NullPointerException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
-        if (messageBytes == null || psBytes == null) {
-            throw new NullPointerException();
-        }
-
-        final String ALGORITHM = "AES";
-        final String ALGORITHM_STR = "AES/ECB/PKCS7Padding";
-        Cipher cipher = Cipher.getInstance(ALGORITHM_STR);
-        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(psBytes, ALGORITHM));
-        return cipher.doFinal(messageBytes);
-
+        return encrypt("AES/ECB/PKCS7Padding", messageBytes, psBytes);
     }
 
     public static byte[] decrypt(byte[] messageBytes, byte[] psBytes) throws NullPointerException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+        return decrypt("AES/ECB/PKCS7Padding", messageBytes, psBytes);
+    }
+
+    public static byte[] encrypt(String algorithm, byte[] messageBytes, byte[] psBytes) throws NullPointerException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
         if (messageBytes == null || psBytes == null) {
             throw new NullPointerException();
         }
 
         final String ALGORITHM = "AES";
-        final String ALGORITHM_STR = "AES/ECB/PKCS7Padding";
-        Cipher cipher = Cipher.getInstance(ALGORITHM_STR);
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(psBytes, ALGORITHM));
+        return cipher.doFinal(messageBytes);
+    }
+
+    public static byte[] decrypt(String algorithm, byte[] messageBytes, byte[] psBytes) throws NullPointerException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+        if (messageBytes == null || psBytes == null) {
+            throw new NullPointerException();
+        }
+
+        final String ALGORITHM = "AES";
+        Cipher cipher = Cipher.getInstance(algorithm);
         cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(psBytes, ALGORITHM));
         return cipher.doFinal(messageBytes);
     }
@@ -201,24 +208,27 @@ public class AESHelper {
         return false;
     }
 
-    @Nullable
     public static byte[] encryptBytes(byte[] key, byte[] sourceBytes) {
         ByteArrayInputStream in = null;
+        ByteArrayOutputStream out = null;
         try {
             in = new ByteArrayInputStream(sourceBytes);
+            out = new ByteArrayOutputStream();
 
             Cipher cipher = initAESCipher(key, Cipher.ENCRYPT_MODE);
-            if (cipher == null)
-                return null;
             BufferedInputStream bis = new BufferedInputStream(in);
+            BufferedOutputStream bos = new BufferedOutputStream(out);
             byte[] buffer = new byte[1024 * 100];
-            byte[] destBuffer = new byte[sourceBytes.length];
-            int nRead;
-            while ((nRead = bis.read(buffer)) != -1) {
-                System.arraycopy(cipher.doFinal(buffer), 0, destBuffer, 0, nRead);
+            int length;
+            while ((length = bis.read(buffer)) != -1) {
+                byte[] data = cipher.doFinal(buffer, 0, length);
+                Log.d("demo", "encryptBytes doFinal length->" + data.length);
+                bos.write(data, 0, data.length);
             }
+            byte[] data = out.toByteArray();
             bis.close();
-            return destBuffer;
+            bos.close();
+            return data;
         } catch (FileNotFoundException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (IOException e) {
@@ -235,28 +245,40 @@ public class AESHelper {
             } catch (IOException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
+
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
         }
         return null;
     }
 
-    @Nullable
     public static byte[] decryptBytes(byte[] key, byte[] sourceBytes) {
         ByteArrayInputStream in = null;
+        ByteArrayOutputStream out = null;
         try {
             in = new ByteArrayInputStream(sourceBytes);
+            out = new ByteArrayOutputStream();
 
             Cipher cipher = initAESCipher(key, Cipher.DECRYPT_MODE);
-            if (cipher==null)
-                return null;
             BufferedInputStream bis = new BufferedInputStream(in);
+            BufferedOutputStream bos = new BufferedOutputStream(out);
+
             byte[] buffer = new byte[1024 * 100 + 16];
-            byte[] destBuffer = new byte[sourceBytes.length];
-            int nRead;
-            while ((nRead = bis.read(buffer)) != -1) {
-                System.arraycopy(cipher.doFinal(buffer), 0, destBuffer, 0, nRead);
+            int length;
+            while ((length = bis.read(buffer)) != -1) {
+                byte[] data = cipher.doFinal(buffer, 0, length);
+                Log.d("demo", "decryptBytes doFinal length->" + data.length);
+                bos.write(data, 0, data.length);
             }
+            byte[] data = out.toByteArray();
             bis.close();
-            return destBuffer;
+            bos.close();
+            return data;
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (BadPaddingException e) {
@@ -265,13 +287,45 @@ public class AESHelper {
             e.printStackTrace();
         } finally {
             try {
-                if (in!=null){
+                if (in != null) {
                     in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+            try {
+                if (out != null) {
+                    out.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
         }
         return null;
+    }
+
+    /**
+     * 随机生成密码
+     *
+     * @param length 密码的长度
+     * @return 最终生成的密码
+     */
+    public static String generatePassword(int length) {
+        // 最终生成的密码
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            // 随机生成0或1，用来确定是当前使用数字还是字母 (0则输出数字，1则输出字母)
+            int charOrNum = random.nextInt();
+            if (charOrNum == 1) {
+                // 随机生成0或1，用来判断是大写字母还是小写字母 (0则输出小写字母，1则输出大写字母)
+                password.append(random.nextInt(26) + random.nextInt(2) == 1 ? 65 : 97);
+            } else {
+                // 生成随机数字
+                password.append(random.nextInt(10));
+            }
+        }
+        return password.toString();
     }
 }
